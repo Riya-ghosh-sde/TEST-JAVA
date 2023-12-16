54e8ab8ecfc8b395d734c90bd1c97e8e0ede9aec
Riya Ghosh <riaghosh419@gmail.com>
2023-12-16 11:53:18 +0530

    Print Days Of Week

---
+++ PrintDaysOfWeek.java
@@ -0,0 +1,11 @@
+public class PrintDaysOfWeek {
+    public static void main(String[] args) {
+        String[] daysOfWeek = {"Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"};
+
+        System.out.println("Days of the week:");
+
+        for (String day : daysOfWeek) {
+            System.out.println(day);
+        }
+    }
+}
